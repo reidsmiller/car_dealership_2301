f1e42b601bd153dec9d3c67815794a42efe46866
Reid Miller <reid.s.miller@gmail.com>
2023-02-08 09:44:21 -0700

    test: Add iteration 3 tests to dealership_spec

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -29,4 +29,69 @@ RSpec.describe 'Dealership' do
       expect(dealership.inventory_count).to eq(2)
     end
   end
+
+  describe '#Iteration 3' do
+    it 'can tell if it has inventory' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+      expect(dealership.has_inventory?).to be false
+    
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+
+      expect(dealership.has_inventory?).to be true
+    end
+
+    it 'can sort cars by make' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+
+      expect(dealership.cars_by_make("Toyota").length).to eq (2)
+      expect(dealership.cars_by_make("Ford").length).to eq (1)
+    end
+
+    it 'can get total value of dealership' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+
+      expect(dealership.total_value).to eq(156000)
+    end
+
+    it 'can convert dealership details into a hash'
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+
+    expect(dealership.details).to eq({"total_value" => 156000, "address" => "123 Main Street"})
+  end
 end
