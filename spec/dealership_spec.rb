b10e7cfdfcc40ec1db6c5d0fb59a7ce132bbf6d6
Reid Miller <reid.s.miller@gmail.com>
2023-02-08 09:30:50 -0700

    test: Add iteration 2 dealership tests

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -10,8 +10,22 @@ RSpec.describe 'Dealership' do
       expect(dealership).to be_instance_of(Dealership)
     end
 
-    it 'has an inventory' do
-      
+    it 'has an inventory and inventory count' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+      expect(dealership.inventory).to eq([])
+      expect(dealership.inventory_count).to eq(0)
+    end
+
+    it 'can add cars to inventory' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+
+      expect(dealership.inventory_count).to eq(2)
     end
   end
 end
